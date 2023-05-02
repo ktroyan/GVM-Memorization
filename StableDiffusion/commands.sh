75e7dbe2547fef645a61b45d4d3963f6bed2d1ce
MrPositron <35646knk@gmail.com>
2023-05-02 19:08:50 +0200

    add username as arg

--- StableDiffusion/commands.sh
+++ StableDiffusion/commands.sh
@@ -1,4 +1,15 @@
-USERNAME="nkoisheke"
+#!/bin/bash
+
+# pass username as first argument, otherwise default to "user"
+if [ ! -z "$1" ]; then
+    USERNAME="$1"
+else
+    USERNAME="user"
+fi
+
+echo "Using username: $USERNAME"
+
+
 DIRECTORY=generative_models
 ssh $USERNAME@euler mkdir -p $DIRECTORY
 
@@ -8,5 +19,3 @@ module load gcc/8.2.0 python_gpu/3.10.4 r/4.0.2 git-lfs/2.3.0 eth_proxy npm/6.14
 pip install --upgrade torch torchvision torchaudio
 pip install --upgrade xformers
 ENDSSH
-
-scp sd_diffusers.ipynb $USERNAME@euler:/cluster/home/$USERNAME/$DIRECTORY
